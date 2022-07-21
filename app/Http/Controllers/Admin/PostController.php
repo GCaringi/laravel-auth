7a46dc50f52709065c3b9ac4eddc300c54b278ed
GCaringi <giannicaringi94@gmail.com>
2022-07-21 08:04:16 +0200

    Add CRUD missing validation

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -96,7 +96,7 @@ class PostController extends Controller
     {
         $post->delete();
 
-        return redirect(route('admin.home'));
+        return redirect(route('admin.posts.index'));
     }
 
 
